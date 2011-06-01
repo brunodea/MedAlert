0f1990773129369d69056a314fba5aa584cd7909
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2011-06-01 11:53:59 -0300

    TextField do password com 'asteriscos'.

--- MedAlert/LoginViewController.m
+++ MedAlert/LoginViewController.m
@@ -23,7 +23,6 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
     }
     return self;
 }
@@ -46,7 +45,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    passwordTextField.secureTextEntry = YES;
 }
 
 - (void)viewDidUnload
