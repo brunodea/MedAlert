8e4393e62255a8db043a8814ef873cc5717e56ab
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2011-06-01 10:50:52 -0300

    LoginViewController adicionado.

---
+++ MedAlert/LoginViewController.h
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.h
+//  MedAlert
+//
+//  Created by Bruno Romero de Azevedo on 01/06/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LoginViewController : UIViewController {
+    IBOutlet UILabel *usernameLabel;
+    IBOutlet UILabel *passwordLabel;
+    
+    IBOutlet UITextField *usernameTextField;
+    IBOutlet UITextField *passwordTextField;
+    
+    IBOutlet UIButton *loginButton;
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *usernameLabel;
+@property (nonatomic, retain) IBOutlet UILabel *passwordLabel;
+
+@property (nonatomic, retain) IBOutlet UITextField *usernameTextField;
+@property (nonatomic, retain) IBOutlet UITextField *passwordTextField;
+
+@property (nonatomic, retain) IBOutlet UIButton *loginButton;
+
+@end
