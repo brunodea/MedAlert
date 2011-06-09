6317f42358d63516325d3f12850f1846c2988f5f
Bruno Romero de Azevedo <brunodea@inf.ufsm.br>
2011-06-09 14:54:02 -0300

    libsqlite3.dylib removido

---
+++ MedAlert/Database/UserDAO.h
@@ -0,0 +1,16 @@
+//
+//  UserDAO.h
+//  MedAlert
+//
+//  Created by Bruno Romero de Azevedo on 09/06/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <sqlite3.h>
+
+@interface UserDAO : NSObject {
+    
+}
+
+@end
---
+++ MedAlert/Database/UserDAO.m
@@ -0,0 +1,13 @@
+//
+//  UserDAO.m
+//  MedAlert
+//
+//  Created by Bruno Romero de Azevedo on 09/06/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserDAO.h"
+
+@implementation UserDAO
+
+@end
